42dad286a08bd4f546824b6eac2f540b675dbaa6
Anik Mohammad <anik7303@gmail.com>
2023-06-08 17:00:48 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import logo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="app">
+      <a href="https://react.dev" target="_blank">
+        <img src={logo} className="logo" alt="React logo" />
+      </a>
+      <h1>React App</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
+
+export default App;
