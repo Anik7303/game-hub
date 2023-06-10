850b6f57462d856da904a23f1637640a225463f5
Anik Mohammad <anik7303@gmail.com>
2023-06-10 12:51:59 +0600

    intall chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import logo from "./assets/react.svg";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="app">
-      <a href="https://react.dev" target="_blank">
-        <img src={logo} className="logo" alt="React logo" />
-      </a>
-      <h1>React App</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-    </div>
-  );
+  return <Button colorScheme="cyan">Button</Button>;
 }
 
 export default App;
