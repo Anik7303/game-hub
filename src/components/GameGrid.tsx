bd4cc5785755e3198fe7d771be02dd100e9916b1
Anik Mohammad <anik7303@gmail.com>
2023-06-20 00:02:44 +0600

    create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,37 +1,10 @@
-import { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
 
-import apiClient, { CanceledError } from "../services/api-client";
-
-type Game = {
-  id: number;
-  name: string;
-};
-
-type FetchGamesResponse = {
-  count: number;
-  results: Game[];
-};
+import useGames from "../hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    const controller = new AbortController();
-
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      });
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
   return (
     <>
       {error && <Text color="red.500">{error}</Text>}
