9aa6fefd4a2c0ed9804f5f25175708a6605a6999
Anik Mohammad <anik7303@gmail.com>
2023-06-19 21:26:08 +0600

    build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="coral">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="yellowgreen">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} alt="Logo" boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
   );
 }
