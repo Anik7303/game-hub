fb381f9138545cf9fb2f9e62579736c676c4a310
Anik Mohammad <anik7303@gmail.com>
2023-06-20 21:14:18 +0600

    fix import statements

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios, { CanceledError } from "axios";
+import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
@@ -6,5 +6,3 @@ export default axios.create({
     key: "8d9fb6be6f9a44299a7425b869b23e06",
   },
 });
-
-export { CanceledError };
