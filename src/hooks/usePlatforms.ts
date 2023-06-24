dcf1098f35383206649cdd3fdba5aa2e1826d1df
Anik Mohammad <anik7303@gmail.com>
2023-06-24 18:57:04 +0600

    refactor: use react-query to manage platform list

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export type Platform = {
   id: number;
@@ -6,6 +9,15 @@ export type Platform = {
   slug: string;
 };
 
-const usePlatforms = () => ({ data: platforms, error: null, isLoading: false });
+const usePlatforms = () =>
+  useQuery<Platform[], Error>({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/list/parents")
+        .then((res) => res.data.results),
+    staleTime: 86_400_000, // 24 * 60 * 60 * 1000 ms = 24h
+    initialData: platforms,
+  });
 
 export default usePlatforms;
